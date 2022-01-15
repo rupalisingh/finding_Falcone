fe5180c4ca41988c5307e65aa6b4f561297b7e6e
Rupali Singh <rupalisingh2304@gmail.com>
2022-01-15 17:38:12 +0530

    Created base UI

---
+++ src/Main.js
@@ -0,0 +1,251 @@
+import React from "react";
+import './Main.css'
+function Main() {
+  return (
+    <>
+      <div className="Header">Finding Falcone</div>
+      <div className="main-body">
+        <div className="body-header">Select Planet you want to Search in:</div>
+        <div className="destination_vehicle">
+          <div className="select-destination">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <button
+                  class="btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Destination 1
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <div role="separator" class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </div>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Text input with dropdown button"
+              />
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio1"
+                  name="optradio"
+                  value="option1"
+                  checked
+                />
+                Option 1<label class="form-check-label" for="radio1"></label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio2"
+                  name="optradio"
+                  value="option2"
+                />
+                Option 2<label class="form-check-label" for="radio2"></label>
+              </div>
+              <div class="form-check">
+                <input type="radio" class="form-check-input" disabled />
+                Option 3<label class="form-check-label"></label>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <button
+                  class="btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Destination 1
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <div role="separator" class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </div>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Text input with dropdown button"
+              />
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio1"
+                  name="optradio"
+                  value="option1"
+                  checked
+                />
+                Option 1<label class="form-check-label" for="radio1"></label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio2"
+                  name="optradio"
+                  value="option2"
+                />
+                Option 2<label class="form-check-label" for="radio2"></label>
+              </div>
+              <div class="form-check">
+                <input type="radio" class="form-check-input" disabled />
+                Option 3<label class="form-check-label"></label>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <button
+                  class="btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Destination 3
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <div role="separator" class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </div>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Text input with dropdown button"
+              />
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio1"
+                  name="optradio"
+                  value="option1"
+                  checked
+                />
+                Option 1<label class="form-check-label" for="radio1"></label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio2"
+                  name="optradio"
+                  value="option2"
+                />
+                Option 2<label class="form-check-label" for="radio2"></label>
+              </div>
+              <div class="form-check">
+                <input type="radio" class="form-check-input" disabled />
+                Option 3<label class="form-check-label"></label>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <button
+                  class="btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Destination 4
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <div role="separator" class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </div>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="Text input with dropdown button"
+              />
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio1"
+                  name="optradio"
+                  value="option1"
+                  checked
+                />
+                Option 1<label class="form-check-label" for="radio1"></label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="radio2"
+                  name="optradio"
+                  value="option2"
+                />
+                Option 2<label class="form-check-label" for="radio2"></label>
+              </div>
+              <div class="form-check">
+                <input type="radio" class="form-check-input" disabled />
+                Option 3<label class="form-check-label"></label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type= "button" className="btn btn-primary">Find Falcone</button>
+      </div>
+    </>
+  );
+}
+
+export default Main;
